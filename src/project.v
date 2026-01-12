3bd7224c3f0c9db465a64a441baeb7b1d4be1f9a
Aadith Yadav G <aadith.ygalu@gmail.com>
2026-01-12 12:27:39 +0800

    Fixed warnings

--- src/project.v
+++ src/project.v
@@ -21,13 +21,15 @@ module tt_um_hamming_code_13_8 (
   assign uio_out[7:2] = 6'b0;
 
   reg [12:0] dwp;
+  wire [4:0] parity_bits = uio_in[6:2];  // Avoid circular combinational logic 
+
   always @(*) begin
     // Hamming Mapping for 13 bits
-    dwp[0] = uio_in[2]; // Overall Parity P0
-    dwp[1] = uio_in[3]; // P0001
-    dwp[2] = uio_in[4]; // P0010
-    dwp[4] = uio_in[5]; // P0100
-    dwp[8] = uio_in[6]; // P1000
+    dwp[0] = parity_bits[0]; // Overall Parity P0
+    dwp[1] = parity_bits[1]; // P0001
+    dwp[2] = parity_bits[2]; // P0010
+    dwp[4] = parity_bits[3]; // P0100
+    dwp[8] = parity_bits[4]; // P1000
 
     dwp[3]     = ui_in[0];
     dwp[7:5]   = ui_in[3:1];
@@ -56,8 +58,11 @@ module tt_um_hamming_code_13_8 (
     end
   end
 
-  assign uio_out[1] = (syndrome != 0 && !overall_parity);
-  assign uio_out[0] = overall_parity | uio_out[1]; 
+  wire double_flag = (syndrome != 0 && !overall_parity);
+  wire single_flag = overall_parity | double_flag;
+
+  assign uio_out[1] = double_flag;
+  assign uio_out[0] = single_flag;
 
   // Map back to outputs
   assign uo_out[0]   = corrected[3];
@@ -65,6 +70,6 @@ module tt_um_hamming_code_13_8 (
   assign uo_out[7:4] = corrected[12:9];
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, rst_n, uio_in[7], uio_in[1:0], corrected[8], corrected[4], corrected[2:0]};
 
 endmodule
